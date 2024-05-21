6518a40aa058ab9fe31908c7df108ab2778537d6
LUCAS HENRIQUE DA ROSA BENEVENUTO <lucas_benevenuto@estudante.sesisenai.org.br>
2024-05-21 20:55:02 -0300

    meu primeiro commit

---
+++ aula02.js
@@ -0,0 +1,8 @@
+var nome = "lucas benevenuto"
+var idade = 30
+var eprofessor = true
+var datadenacimento
+
+document.write(typeof nome,'<br/>')
+document.write(typeof datadenacimento,'<br/>')
+document.write(typeof eprofessor)
---
+++ src/aula01/Aula01.java
@@ -0,0 +1,28 @@
+
+package aula01;
+
+import java.util.Scanner;
+
+
+public class Aula01 {
+
+
+    public static void main(String[] args) {
+        int idade = 26;
+        String nome = "lucas benevenuto";
+        char genero = 'm';
+        String.
+        String.
+        
+         double salario = 100.50;
+         
+        }
+            
+        }
+            
+        
+                    
+                   
+         
+            
+        
